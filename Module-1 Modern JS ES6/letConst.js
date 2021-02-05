f392ec3af04ec0c0de148d4d26fff5ca667432ae
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-05 22:55:20 +0600

    Let, const, array declared with const, object declared with const

---
+++ Module-1 Modern JS ES6/letConst.js
@@ -0,0 +1,28 @@
+const hubby = "Samiul Sheikh";
+// hubby = "Sami Sheikh";
+console.log(hubby);
+
+const numbers = [15, 35];
+numbers[1] = 50;
+numbers.push(20);
+// numbers = [60, 70, 80, 100];
+console.log(numbers);
+
+const hero = { name: "Shahrukh Khan", phone: 45678 };
+
+let patientName = "Nobel Sheikh";
+patientName = "Lingcon Sheikh"
+console.log(patientName);
+
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(i);
+
+// when using let we can not access outside of scope(does not lick)
+// let sum = 0;
+// for (let i = 0; i < 10; i++) {
+//     sum = sum + i;
+// }
+// console.log(i);
