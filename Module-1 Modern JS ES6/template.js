086b81b6757c5d854a4aa3e0d20683cb26d38750
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-05 23:02:27 +0600

    Template string, multiple line string

---
+++ Module-1 Modern JS ES6/template.js
@@ -0,0 +1,18 @@
+const firstName = "Justin";
+const lastName = "Bibber";
+const fullName = firstName + ' ' + lastName + ' ' + "is a singer";
+// using template
+const fullName2 = `${firstName} ${20 + 30 + 50} is a good boy`;
+console.log(fullName);
+console.log(fullName2);
+
+// another example
+const multiLine = "I love u\n"
+    + "I miss u\n"
+    + "I need u";
+console.log(multiLine);
+
+const multiLine2 = `You don't receive my call!
+okk, no need!
+break up!!`;
+console.log(multiLine2);
