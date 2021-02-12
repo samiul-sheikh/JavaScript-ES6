d917df55a7c80acfaf328463879ac17a82933443
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-12 22:27:21 +0600

    double equal (==) vs triple equal (===), implicit conversion

---
+++ Module-2 Intermediate JS, Interview questions/double-vs-triple-equal.js
@@ -0,0 +1,37 @@
+// == => check only value not type;
+// === => check value also value type;
+
+/*
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition true");
+}
+else {
+    console.log("condition false");
+}
+*/
+
+
+// true => 1; false => 0;
+
+const first = 1;
+const second = true;
+if (first == second) {
+    console.log("condition true");
+}
+else {
+    console.log("condition false");
+}
+
+
+/*
+const first = 0;
+const second = false;
+if (first === second) {
+    console.log("condition true");
+}
+else {
+    console.log("condition false");
+}
+*/
