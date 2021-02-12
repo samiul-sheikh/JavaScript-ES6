0580a049396789f87a09b61143e36c0ca845ebc9
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-12 22:23:53 +0600

    Null Vs Undefined, different ways you will get undefined

---
+++ Module-2 Intermediate JS, Interview questions/null-vs-undefined.js
@@ -0,0 +1,41 @@
+// if we not declare a variable value its undefined
+// let user;
+// console.log(user);
+
+
+// if we explicitly do not return/ write only return its undefined
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(10, 15);
+// console.log(result);
+
+
+// if parameter not receive value also undefined
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+// const result = add(10);
+
+
+// if any property does not stay in object which we call its undefined
+// const user = { name: "samiul", phone: 889997 }
+// console.log(user.gf);
+
+
+// if we set value undefined its also undefined
+// let fun = undefined;
+// console.log(fun);
+
+
+// if we search that element which is not stay in array show undefined
+let ages = [2, 5, 7];
+console.log(ages[10]);
+
+// null = not exist
+
+// function doSomething(x,y){
+//     console.log(y);
+// }
+// doSomething(32);
